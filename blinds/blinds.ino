40fb30349d7e1364e1041b555be0c86a797ae465
Manojav Sridhar <manojav@acoustix.manojav.com>
2019-08-21 13:20:40 -0400

    update year on copyright

--- blinds/blinds.ino
+++ blinds/blinds.ino
@@ -1,7 +1,7 @@
 
 /*
   Yet another Blind Minder - Arudino blind controller
-  Copyright (C) 2018 vajonam
+  Copyright (C) 2019 vajonam
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
